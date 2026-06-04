38322f4b8a304bc50f00f21fcd2ba1ee3d59d3fb
Freddy <fredsotoucsm@gmail.com>
2026-06-04 13:15:15 -0500

    prueba con server linux

--- cliente_emisor.py
+++ cliente_emisor.py
@@ -2,7 +2,7 @@ import socket
 import melgar_cifrado as cipher
 
 CLUB_SIGNATURE = "FBC Melgar de Arequipa"
-HOST = '127.0.0.1'
+HOST = '172.17.0.1'
 PORT = 65432
 CLAVE_SECRETA = "acceso_telematico_01"
 
@@ -11,16 +11,18 @@ def enviar_mensaje(mensaje_original):
         try:
             s.connect((HOST, PORT))
         except ConnectionRefusedError:
-            print("[CLIENTE] Error: El servidor receptor no está en línea.")
+            print("[ERROR] El servidor receptor en Kali Linux no está en línea o el puerto está bloqueado.")
             return
 
-        print(f"[CLIENTE] Texto original: {mensaje_original}")
+        print(f"\n[PROCESO] Texto original: {mensaje_original}")
         payload_cifrado = cipher.encrypt_data(mensaje_original, CLAVE_SECRETA, CLUB_SIGNATURE)
         
-        print(f"[CLIENTE] Carga útil procesada enviada a la red: {payload_cifrado}")
+        print(f"[PROCESO] Carga útil encriptada lista para transmisión:\n{payload_cifrado}")
         s.sendall(payload_cifrado.encode('utf-8'))
+        print("[SISTEMA] Paquete TCP enviado con éxito.")
 
 if __name__ == "__main__":
+    print("--- TERMINAL DE COMUNICACIÓN ENCRIPTADA ---")
     while True:
         mensaje = input("\nIngrese mensaje a transmitir (o 'salir' para terminar): ")
         if mensaje.lower() == 'salir':
